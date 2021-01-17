10c1ef3797b1108a8ef8d9eac3689096ef7f5631
lucasbbs <lucasbbs@live.fr>
2021-01-17 12:18:30 -0300

    "Final Commit trying to deploy everythin up to heroku app"

--- src/main/java/life/belarusian/crud/saladereuniao/saladereuniao/SaladereuniaoApplication.java
+++ src/main/java/life/belarusian/crud/saladereuniao/saladereuniao/SaladereuniaoApplication.java
@@ -2,12 +2,26 @@ package life.belarusian.crud.saladereuniao.saladereuniao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class SaladereuniaoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SaladereuniaoApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
 	}
 
 }
